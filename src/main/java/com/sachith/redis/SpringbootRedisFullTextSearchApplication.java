5b895957bf760299713e9ccb0f973964c50ccf7c
Sachith <sachithlakmal@icloud.com>
2022-08-07 08:54:46 +0530

    Initial commit

---
+++ src/main/java/com/sachith/redis/SpringbootRedisFullTextSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sachith.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisFullTextSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisFullTextSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachith/redis/SpringbootRedisFullTextSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachith.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisFullTextSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
